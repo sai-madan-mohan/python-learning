95599e276455cb6c5d147413d0ecb853be877820
Sai Madan Mohan <chalamalasetti27@gmail.com>
2025-06-06 15:00:08 +0530

    programs

--- practice.py
+++ practice.py
@@ -1,29 +1,39 @@
-# accessing an element in a list
-a=[1,2,3,4,5]
-print(a[2])
+# # accessing an element in a list
+# a=[1,2,3,4,5]
+# print(a[2])
 
-# modify an element in a lissy
-a=[1,2,3,4,5]
-a[3]=10
-print(a)
-# adding an element in a alist
-a=[1,10,12,34]
-a.append(45)
-print(a)
-a.extend([38,44])
-print(a)
-# remoiving an element in a list
-a=[1,12,32,12,25]
-a.remove(12)
-a.pop()
-print(a)
+# # modify an element in a lissy
+# a=[1,2,3,4,5]
+# a[3]=10
+# print(a)
+# # adding an element in a alist
+# a=[1,10,12,34]
+# a.append(45)
+# print(a)
+# a.extend([38,44])
+# print(a)
+# # remoiving an element in a list
+# a=[1,12,32,12,25]
+# a.remove(12)
+# a.pop()
+# print(a)
 
-# slicing a list
-a=[1,2,3,4,5]
-print(a[1:4])
+# # slicing a list
+# a=[1,2,3,4,5]
+# print(a[1:4])
 
-# copying a list
-a=[1,2,3,4,5]
-b=a.copy()
-print(b)
+# # copying a list
+# a=[1,2,3,4,5]
+# b=a.copy()
+# print(b)
+
+a=(10,13,15,23,24,32)
+print(a[:3])
+
+
+a=(10,20)
+b=a
+print(id(a))
+print(id(b))
+print(a == b)
 
