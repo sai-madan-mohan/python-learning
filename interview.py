a2d5a6fabf65d20a7494d4b274d901c9bd001f29
Sai Madan Mohan <chalamalasetti27@gmail.com>
2025-06-08 11:23:36 +0530

    programs

---
+++ interview.py
@@ -0,0 +1,88 @@
+# print("hello world")
+
+# a=2
+# if a>5:
+#     print("a is greater than 5")
+# else:
+#     print("a is not greater than 5")
+
+# x=5
+# y="hello"
+
+# print(x)
+# print(y)
+
+# print(type(x))
+
+
+# x,y,z="orange","apple","banana"
+
+# x=y=z="oranges"
+
+# print(x)
+# print(y)
+# print(z)
+
+# x=[1,2,3]
+# x[0]=10
+# print(x)
+
+# x=4
+# z=44
+# print( x + z )
+
+# x="python"
+
+# def my_func():
+#     print(x)
+# my_func()
+
+# x="apple"
+# print("b" not in x)
+
+# b="Hello , World"
+# print(b[-7:])
+
+# a="Hello"
+# print(a.upper())
+
+# a="    hello world             "
+# print(a.strip())
+
+# b="MADAN"
+# print(b.replace("D","S"))
+
+# thislist =["apple",1,2,5,1,4534,231,213456765432,221]
+# i=0
+# while i< len(thislist):
+#     print(thislist[i])
+#     i=i+1
+
+# squares =[x**2 for x in range(10)]
+# print(squares)
+
+# fruitslist=["apple", "banana", "cherry"]
+# name=[]
+# for x in fruitslist:
+#     if "a" in x:
+#         name.append(x)
+# print(name)
+
+# a=[1,2,3,4,4,4,4,4,4,1,1,2,1,1,3,2,1]
+# a.insert(1, 10)
+# print(a)
+
+i=0
+while i<8:
+    i=i+1
+    if i==3:
+        continue    
+    print(i)
+
+i=0
+while i<8:
+    print(i)
+    i=i+1
+    
+    if i==3:
+        break
